b1a884109453a5089dae7a79fe6212dba8ce2f3a
Sanjay <sanjayrpatil1984@gmail.com>
2023-11-30 15:06:04 +0530

    flask version changes

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'SQLALCHEMY_DATABASE_URI'
 db = SQLAlchemy(app)
 
 
